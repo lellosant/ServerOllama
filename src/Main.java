d5c4d2945a8617275b1dd24a5e4d1e01c5cc23b5
lellosant <leonardosantucci99@gmail.com>
2025-09-10 13:31:14 +0200

    ho modificato il main in modo che crei il file di configurazione se non trovato

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import it.uniroma2.ollama.config.OllamaConfig;
 import it.uniroma2.ollama.server.OllamaServer;
 
 import java.io.File;
+import java.io.PrintWriter;
 
 public class Main {
 
@@ -10,10 +11,19 @@ public class Main {
         try {
             File configFile = new File("config.txt");
             if (!configFile.exists()) {
-                System.out.println("File config.txt non trovato, uso valori di default.");
-                // Puoi creare la Config con valori di default manualmente
-                // oppure terminare l'applicazione
-            } else {
+                System.out.println("File config.txt non trovato. Creo un file di configurazione di default...");
+
+                try (PrintWriter writer = new PrintWriter(configFile)) {
+                    writer.println("port=8080");
+                    writer.println("model=llama3");
+                    writer.println("ollama_endpoint=http://localhost:11434/api/generate");
+                    writer.println("request_timeout=30");
+                }catch (Exception e){
+                    System.err.println("errore nella creazione del file di configurazione :" +  e.getMessage());
+                }
+
+            }
+            try{
                 OllamaConfig config = new OllamaConfig("config.txt");
                 int port = config.getInt("port", 8080);
                 String model = config.getString("model", "llama3");
@@ -21,10 +31,10 @@ public class Main {
                 int timeout = config.getInt("request_timeout",30);
                 OllamaServer server = new OllamaServer(port, model,ollamaEndpoint,timeout);
                 server.start();
-            }
-
-        } catch (Exception e) {
+            } catch (Exception e) {
             e.printStackTrace();
         }
- }
-}
+ }catch (Exception e){
+            e.printStackTrace();
+        }
+}}
